22569003096a537f6d4d43a6d9db5106ceee9ead
Lennart Gamradt <lennart.gamradt@nordakademie.de>
2020-12-06 07:00:10 +0100

    Fix import format

--- client/src/app/rooms/room-list/room-list.component.ts
+++ client/src/app/rooms/room-list/room-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Room} from "../shared/room";
+import {Room} from '../shared/room';
 
 @Component({
     selector: 'app-room-list',
